591204d19d8dd46c70466dfad27934431ac1bb59
Eric <eric.j.blom@gmail.com>
2019-04-27 23:17:41 -0400

    added chart, and story and lots of stuff

---
+++ index.js
@@ -0,0 +1,158 @@
+// Setup svg using Bostock's margin convention
+
+var element = document.getElementById('chart-card');
+var positionInfo = element.getBoundingClientRect();
+// var h = positionInfo.height;
+var w = positionInfo.width;
+
+var margin = {top: 20, right: 20, bottom: 40, left: 40};
+
+var width = w - margin.left - margin.right,
+    height = (w * 2/3) - margin.top - margin.bottom;
+
+var svg = d3.select("#chart")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+d3.csv("data/bird_totals.csv", function(error, data) {
+    if (error) throw error;
+
+  var parse = d3.time.format("%Y").parse;
+
+
+  // Transpose the data into layers
+  var dataset = d3.layout.stack()(["ducks", "geese", "mergansers", "coots"].map(function(bird) {
+    return data.map(function(d) {
+      return {x: parse(d.year), y: +d[bird]};
+    });
+  }));
+
+
+  // Set x, y and colors
+  var x = d3.scale.ordinal()
+    .domain(dataset[0].map(function(d) { return d.x; }))
+    .rangeRoundBands([10, width-10], 0.02);
+
+  var y = d3.scale.linear()
+    .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
+    .range([height, 0]);
+
+  var colors = ["#1b9e77", "#d95f02", "#7570b3", "#e7298a"];
+
+
+
+  // Define and draw axes
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(5)
+    .tickSize(-width, 0, 0)
+    .tickFormat( function(d) { return d } );
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%Y"));
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+
+  // add axes labesl
+  svg.append("text")
+    .attr("class", "label")
+    .attr("y", height + 30)
+    .attr("x", width/2)
+    .style("text-anchor", "middle")
+    .text("Hunt Year")
+
+  svg.append("text")
+      .attr("class", "label")
+     .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left )
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Birds Harvested");
+
+  // Create groups for each series, rects for each segment 
+  var groups = svg.selectAll("g.cost")
+    .data(dataset)
+    .enter().append("g")
+    .attr("class", "cost")
+    .style("fill", function(d, i) { return colors[i]; });
+
+  var rect = groups.selectAll("rect")
+    .data(function(d) { return d; })
+    .enter()
+    .append("rect")
+    .attr("x", function(d) { return x(d.x); })
+    .attr("y", function(d) { return y(d.y0 + d.y); })
+    .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+    .attr("width", x.rangeBand())
+    .on("mouseover", function() { tooltip.style("display", null); })
+    .on("mouseout", function() { tooltip.style("display", "none"); })
+    .on("mousemove", function(d) {
+      var xPosition = d3.mouse(this)[0] - 15;
+      var yPosition = d3.mouse(this)[1] - 25;
+      tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+      tooltip.select("text").text(d.y);
+    });
+
+
+  // Draw legend
+  var legend = svg.selectAll(".legend")
+    .data(colors)
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(30," + i * 19 + ")"; });
+   
+  legend.append("rect")
+    .attr("x", width - 100)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", function(d, i) {return colors.slice().reverse()[i];});
+   
+  legend.append("text")
+    .attr("x", width - 80)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "start")
+    .text(function(d, i) { 
+      switch (i) { 
+        case 0: return "Coots";
+        case 1: return "Mergansers";
+        case 2: return "Geese";
+        case 3: return "Ducks";
+      }
+    });
+
+
+  // Prep the tooltip bits, initial display is hidden
+  var tooltip = svg.append("g")
+    .attr("class", "tooltip")
+    .style("display", "none");
+      
+  tooltip.append("rect")
+    .attr("width", 30)
+    .attr("height", 20)
+    .attr("fill", "white")
+    .style("opacity", 0.5);
+
+  tooltip.append("text")
+    .attr("x", 15)
+    .attr("dy", "1.2em")
+    .style("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold");
+});
